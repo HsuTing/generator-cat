754269978a6c47e96fc0fb2932481c629e2abfe6
hsuting <hsuting0106@gmail.com>
2017-02-28 09:02:36 +0800

    add script in npm

--- generators/npm/index.js
+++ generators/npm/index.js
@@ -9,7 +9,10 @@ module.exports = class extends Generator {
     // pkg
     const currentPkg = this.fs.readJSON(this.destinationPath('package.json'), {});
     const pkg = extend({
-      main: './lib/index.js'
+      main: './lib/index.js',
+      scripts: {
+        prepublish: 'yarn babel'
+      }
     }, currentPkg);
     this.fs.writeJSON(this.destinationPath('package.json'), pkg);
 
