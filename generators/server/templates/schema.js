2f292913f20a1e0d534a2ff49bcd42064fe0fc6d
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2017-07-27 03:37:26 +0800

    Update schema.js

--- generators/server/templates/schema.js
+++ generators/server/templates/schema.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import fs from 'fs';
+import path from 'path';
 import {
   GraphQLSchema,
   GraphQLObjectType
@@ -12,6 +13,9 @@ let mutation = {};
 fs.readdirSync(__dirname)
   .filter(file => !(file[0] === '.' || file === 'schema.js'))
   .forEach(file => {
+    if(!fs.existsSync(path.resolve(__dirname, `./${file}/index.js`)))
+      return;
+  
     const name = file.replace('.js', '');
     const schema = require(`./${name}`).default || require(`./${name}`);
 
