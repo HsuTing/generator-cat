95499e706ab8062508859c7414a84cf3a80a868e
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2017-08-05 09:57:11 +0800

    Update dataType.js

--- generators/add/templates/schema/dataType.js
+++ generators/add/templates/schema/dataType.js
@@ -14,11 +14,11 @@ export default new GraphQLObjectType({
   name: '<%= name %>',
   description: 'This is the type of the <%= name %>.',
   interfaces: [nodeInterface],
-  fields: () => ({
+  fields: {
     id: globalIdField('<%= name %>'),
     data: {
       type: GraphQLString,
       description: 'This is the data of the <%= name %>.'
     }
-  })
+  }
 });
