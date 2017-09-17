decbb8c9c4e394e52ae08302a601c5dc7f3a71d2
HsuTing <hsuting0106@gmail.com>
2017-09-18 02:22:22 +0800

    modify relay test

--- generators/add/templates/jest/relay.js
+++ generators/add/templates/jest/relay.js
@@ -8,19 +8,18 @@ import <%= name %> from 'components/<%= name %>';
 import environment from 'utils/environment';
 import <%= queryName %>Query, {variables as <%= queryName %>Variables} from 'constants/query/<%= queryName %>Query';
 
+const wrapper = mount(
+  <<%= name %> />
+);
 let server = null;
 
 describe('<%= name %>', () => {
-  beforeAll(() => {
+  beforeAll(async () => {
     server = require('./../../server').default;
+    await relayQueryLookupRender(environment, <%= queryName %>Query, <%= queryName %>Variables)({}, () => {});
   });
 
   it('run', async () => {
-    const wrapper = mount(
-      <<%= name %> />
-    );
-
-    await relayQueryLookupRender(environment, <%= queryName %>Query, <%= queryName %>Variables)({}, () => {});
     expect(wrapper.containsAnyMatchingElements([
       <div>{JSON.stringify({<%= queryName %>: {data: 'query <%= name %>'}})}</div> // eslint-disable-line react/jsx-key
     ])).toBe(true);
