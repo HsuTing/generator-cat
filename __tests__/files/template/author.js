31607d3c9ae4a6487b54597a9ed727469c72de5e
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2017-10-10 04:15:41 -0500

    Update author.js

--- __tests__/files/template/author.js
+++ __tests__/files/template/author.js
@@ -2,7 +2,7 @@
 
 export default checkContent => {
   checkContent(true, '<link rel="author" href="http://hsuting.com/author/">');
-  checkContent(true, '<link rel="copyright" href="http://hsuting.com/copyright/">');
+  checkContent(true, '<link rel="license" href="http://hsuting.com/copyright/">');
   checkContent(true, '<link rel="me" href="http://hsuting.com" type="text/html">');
   checkContent(true, '<link rel="me" href="mailto:hsuting0106@gmail.com">');
 };
